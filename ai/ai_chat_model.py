506ecb86fab5513cf69ed230f96ce5be79f794aa
Remco Zuidweg <0jr.lines0@gmail.com>
2025-10-10 10:04:13 +0200

    fixed not streaming bug

--- ai/ai_chat_model.py
+++ ai/ai_chat_model.py
@@ -2,6 +2,7 @@ import ollama
 from commands.weather_forecast import weather_forecast
 import datetime
 from fastapi import WebSocket
+import asyncio
 
 
 # Functie om modeloutput te verwerken
@@ -25,6 +26,7 @@ async def ai(user_input:str, websocket:WebSocket):
               await websocket.send_text(full_response)
               i += 1
             await websocket.send_text(part['message']['content'])
+            await asyncio.sleep(0)
           full_response += part['message']['content']
         if hoi:
           await ai(execute_model_output(full_response), websocket)
